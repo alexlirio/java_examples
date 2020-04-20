8e85cff1c3562b1dd431470143ca8a3031c7ffd7
Alex Lirio <alexlirioti@gmail.com>
2020-04-20 16:35:57 -0300

    Add more lambda examples
    Add more lambda examples

--- src/main/java/br/com/example/Lambda.java
+++ src/main/java/br/com/example/Lambda.java
@@ -1,7 +1,10 @@
 package br.com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 
 public class Lambda {
 
@@ -41,6 +44,19 @@ public class Lambda {
 		// Lambda forEach example - More Resumed
 		animals.forEach( a -> System.out.println(a.name));
 		
+		// Lambda forEach example to populate a list
+		List<Animal> animalList = Arrays.asList(new Animal("monkey", 5), new Animal("cat", 15), new Animal("dog", 10));
+		List<String> names = new ArrayList<>();
+		animalList.forEach(a -> names.add(a.getName()));
+		System.out.println(names);
+		
+		// Predicare test use example
+		Predicate<Integer> pair = (Integer i) -> i % 2 == 0;
+		System.out.println(pair.test(10));
+		
+		// IntPredicare test use example. The primitive type save CPU processing.
+		IntPredicate odd = (int i) -> i % 2 != 0;
+		System.out.println(odd.test(10));
 	}
 
 }
@@ -54,6 +70,10 @@ class Animal {
 		super();
 		this.name = name;
 		this.age = age;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
